0902a5db67959abb67b1a9d982c3d3480f1d493a
M4LC0LM123 <lukaagni.malovic@gmail.com>
2023-11-04 18:33:36 +0100

    da

--- aengine/ecs/Prefab.cs
+++ aengine/ecs/Prefab.cs
@@ -37,93 +37,107 @@ public class Prefab {
         ParsedObject obj = data.getObject(name);
         
         if (type == typeof(MeshComponent)) {
-            int r = obj.getValue<int>("r");
-            int g = obj.getValue<int>("g");
-            int b = obj.getValue<int>("b");
-            int a = obj.getValue<int>("a");
-            Color color = new Color(r, g, b, a);
-
-            ShapeType shape = (ShapeType) obj.getValue<int>("shape");
-
-            string texturePath = obj.getValue<string>("texture");
-            string terrainPath = obj.getValue<string>("terrain");
-
-            Texture texture = new Texture();
-            Texture terrain = new Texture();
-            
-            if (texturePath != "") {
-                texture = Raylib.LoadTexture(texturePath);
-            }
-            
-            if (terrainPath != "") {
-                terrain = Raylib.LoadTexture(terrainPath);
-            }
-
-            Mesh mesh = new Mesh();
-            Model model = new Model();
-
-            if (shape is ShapeType.BOX) {
-                mesh = Raylib.GenMeshCube(1, 1, 1);
-            } else if (shape is ShapeType.SPHERE) {
-                mesh = Raylib.GenMeshSphere(1, 15, 15);
-            } else if (shape is ShapeType.CYLINDER) {
-                mesh = Raylib.GenMeshCylinder(1, 1, 15);
-            } else if (shape is ShapeType.CONE) {
-                mesh = Raylib.GenMeshCone(1, 1, 15);
-            } else if (shape is ShapeType.TERRAIN) {
-                mesh = Raylib.GenMeshHeightmap(Raylib.LoadImageFromTexture(terrain), Vector3.One);
-            }
-
-            if (shape is ShapeType.MODEL) {
-                model = Raylib.LoadModel(obj.getValue<string>("model"));
-            } else {
-                model = Raylib.LoadModelFromMesh(mesh);
-            }
-            
-            MeshComponent result = new MeshComponent(entity, 
-                mesh, 
-                color
-            );
-
-            result.scale = obj.getValue<int>("scale");
-            
-            result.model = model;
-            result.setTexture(texture);
-
-            return result;
+            return loadMeshComponent(entity, obj);
         }
         
         if (type == typeof(RigidBodyComponent)) {
-            float mass = obj.getValue<float>("mass");
-            ShapeType shape = (ShapeType)obj.getValue<int>("shape");
-            BodyType bodyType = (BodyType) obj.getValue<int>("type");
-
-            string modelPath = obj.getValue<string>("model");
-            string heightmapPath = obj.getValue<string>("heightmap");
-
-            if (modelPath != "") {
-                Model model = Raylib.LoadModel(modelPath);
-
-                return new RigidBodyComponent(entity,
-                    model, mass, bodyType
-                );
-            }
-            
-            if (heightmapPath != "") {
-                Texture heightmap = Raylib.LoadTexture(heightmapPath);
-
-                return new RigidBodyComponent(entity,
-                    heightmap, mass, bodyType
-                );
-            }
-
-            RigidBodyComponent result = new RigidBodyComponent(entity,
-                mass, bodyType, shape
-            );
-
-            return result;
+            return loadRigidBodyComponent(entity, obj);
         }
 
         return null;
     }
+
+    public static Component loadMeshComponent(Entity entity, ParsedObject obj) {
+        int r = obj.getValue<int>("r");
+        int g = obj.getValue<int>("g");
+        int b = obj.getValue<int>("b");
+        int a = obj.getValue<int>("a");
+        Color color = new Color(r, g, b, a);
+
+        ShapeType shape = (ShapeType)obj.getValue<int>("shape");
+
+        string texturePath = obj.getValue<string>("texture");
+        string terrainPath = obj.getValue<string>("terrain");
+
+        Texture texture = new Texture();
+        Texture terrain = new Texture();
+
+        if (texturePath != "") {
+            texture = Raylib.LoadTexture(texturePath);
+        }
+
+        if (terrainPath != "") {
+            terrain = Raylib.LoadTexture(terrainPath);
+        }
+
+        Mesh mesh = new Mesh();
+        Model model = new Model();
+
+        if (shape is ShapeType.BOX) {
+            mesh = Raylib.GenMeshCube(1, 1, 1);
+        }
+        else if (shape is ShapeType.SPHERE) {
+            mesh = Raylib.GenMeshSphere(1, 15, 15);
+        }
+        else if (shape is ShapeType.CYLINDER) {
+            mesh = Raylib.GenMeshCylinder(1, 1, 15);
+        }
+        else if (shape is ShapeType.CONE) {
+            mesh = Raylib.GenMeshCone(1, 1, 15);
+        }
+        else if (shape is ShapeType.TERRAIN) {
+            mesh = Raylib.GenMeshHeightmap(Raylib.LoadImageFromTexture(terrain), Vector3.One);
+        }
+
+        if (shape is ShapeType.MODEL) {
+            model = Raylib.LoadModel(obj.getValue<string>("model"));
+        }
+        else {
+            model = Raylib.LoadModelFromMesh(mesh);
+        }
+
+        MeshComponent result = new MeshComponent(entity,
+            mesh,
+            color
+        );
+
+        result.scale = obj.getValue<int>("scale");
+
+        result.model = model;
+        result.setTexture(texture);
+
+        return result;
+    }
+
+    public static Component loadRigidBodyComponent(Entity entity, ParsedObject obj) {
+        float mass = obj.getValue<float>("mass");
+        ShapeType shape = (ShapeType)obj.getValue<int>("shape");
+        BodyType bodyType = (BodyType) obj.getValue<int>("type");
+
+        string modelPath = obj.getValue<string>("model");
+        string heightmapPath = obj.getValue<string>("heightmap");
+
+        if (modelPath != "") {
+            Model model = Raylib.LoadModel(modelPath);
+ 
+            return new RigidBodyComponent(entity,
+                model, mass, bodyType
+            );
+        }
+            
+        if (heightmapPath != "") {
+            Texture heightmap = Raylib.LoadTexture(heightmapPath);
+
+            return new RigidBodyComponent(entity,
+                heightmap, mass, bodyType
+            );
+        }
+
+        RigidBodyComponent result = new RigidBodyComponent(entity,
+            mass, bodyType, shape
+        );
+
+        return result;
+    }
+    
 }
